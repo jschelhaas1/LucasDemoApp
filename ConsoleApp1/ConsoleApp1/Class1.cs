d414fa8507aee89c01eab12a53845101414afed4
Joel Schelhaas <jschelhaas1@outlook.com>
2020-04-13 14:51:24 -0500

    Commit

---
+++ ConsoleApp1/ConsoleApp1/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Class1
+    {
+        private int Add()
+        {
+            int x = 1;
+            int y = 2;
+            int sum = x + y;
+            return sum;
+        }
+    }
+}
